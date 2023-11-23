68962501ab2af60a489d3a7be1a28364800ba89f
Itatelino Neto <itatelino.oliveira@gmail.com>
2023-11-23 17:00:02 -0300

    Projeto Criado

---
+++ src/main/java/com/itatech/itatechapirest/ItatechapirestApplication.java
@@ -0,0 +1,13 @@
+package com.itatech.itatechapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItatechapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItatechapirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itatech/itatechapirest/ItatechapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itatech.itatechapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItatechapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
