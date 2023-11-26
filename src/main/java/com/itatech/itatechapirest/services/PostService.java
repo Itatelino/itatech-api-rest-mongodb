35ad2e3775a29eb318b32ef76da44b02771ed3fc
Itatelino Neto <itatelino.oliveira@gmail.com>
2023-11-26 19:45:23 -0300

    Acrecentando comentario aos posts

--- src/main/java/com/itatech/itatechapirest/services/PostService.java
+++ src/main/java/com/itatech/itatechapirest/services/PostService.java
@@ -19,4 +19,14 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
+
