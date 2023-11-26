251acfdc95cf0ed988f7e6065f710cf996c2e328
Itatelino Neto <itatelino.oliveira@gmail.com>
2023-11-26 18:32:30 -0300

    Projecao dos dqdos do autor com DTO

--- src/main/java/com/itatech/itatechapirest/config/Instantiation.java
+++ src/main/java/com/itatech/itatechapirest/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.itatech.itatechapirest.domain.Post;
 import com.itatech.itatechapirest.domain.User;
+import com.itatech.itatechapirest.dto.AuthorDTO;
 import com.itatech.itatechapirest.repository.PostRepository;
 import com.itatech.itatechapirest.repository.UserRepository;
 
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 =  new Post(null, sdf.parse("21/03/2023"), "Partiu Viagem", "Vou viajar para Sao Paulo. Abraços!", maria);
-		Post post2 =  new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 =  new Post(null, sdf.parse("21/03/2023"), "Partiu Viagem", "Vou viajar para Sao Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 =  new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
-
 }
