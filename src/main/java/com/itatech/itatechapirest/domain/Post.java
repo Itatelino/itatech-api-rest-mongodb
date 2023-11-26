076f4a15a702b816f9eeeebeea75609c20bf73e3
Itatelino Neto <itatelino.oliveira@gmail.com>
2023-11-26 19:06:35 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/itatech/itatechapirest/domain/Post.java
+++ src/main/java/com/itatech/itatechapirest/domain/Post.java
@@ -2,8 +2,6 @@ package com.itatech.itatechapirest.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -32,6 +30,39 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	
 	public AuthorDTO getAuthor() {
 		return author;
 	}
@@ -39,10 +70,15 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -54,8 +90,11 @@ public class Post implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-	
 }
